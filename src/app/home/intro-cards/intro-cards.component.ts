18f4d367e5bcb7674452f8874b1bc630d76318f2
ilies bourouh <ilies.bourouh@univ-bba.dz>
2019-02-24 20:13:51 +0100

    small changes (text + images)

--- src/app/home/intro-cards/intro-cards.component.ts
+++ src/app/home/intro-cards/intro-cards.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroCardsComponent implements OnInit {
 
-  description = 'هي مبادرة تسعى إلى تقليص فجوة التواصل بين المتبرعين وبنوك الدم حتى تصبح عملية التبرع بالدم أسهل.';
+  description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore';
   intro_cards = [
     { imgURL: '../../assets/images/leaf.png', text: card1_text[0] },
     { imgURL: '../../assets/images/takephoto.png', text: card1_text[1] },
@@ -20,8 +20,8 @@ export class IntroCardsComponent implements OnInit {
 
 }
 const card1_text = [
-  'متبر وتين فخور بكل فرد مبادر بالعطاء على عمله الإنساني. {وَمَنْ أَحْيَاهَا فَكَأنَّمَا أَحْيَا النَّاسَ جَمِيْعَاً} المائدة:٣٢',
-  'أشخاص ينتظرون تبرعك اليوم!',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore',
   'Using just a simple smartphone picture, Plantix image recognition \
           is able to detect more than 240 plant pests & diseases automatically.'
 ];
